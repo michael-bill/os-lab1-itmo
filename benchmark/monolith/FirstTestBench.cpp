dcdca2dea3e815bff4a4c295ca408fef0697d546
Michael-Bill <biloshitskiy_m@mail.ru>
2025-01-18 00:03:30 +0300

    feat: shell and first test benchmark

---
+++ benchmark/monolith/FirstTestBench.cpp
@@ -0,0 +1,18 @@
+#include <benchmark/benchmark.h>
+#include <vector>
+
+// Функция, которую мы будем тестировать
+void BM_VectorPushBack(benchmark::State& state) {
+    for (auto _ : state) {
+        std::vector<int> v;
+        for (int i = 0; i < state.range(0); ++i) {
+            v.push_back(i);
+        }
+    }
+}
+
+// Регистрируем бенчмарк
+BENCHMARK(BM_VectorPushBack)->Range(8, 8<<10);
+
+// Основная функция для запуска бенчмарков
+BENCHMARK_MAIN();
